4986b6e52576667957724a646d01c1299c74c3ec
AKursanov <akursanov@gmail.com>
2020-12-11 09:13:48 +0500

    Библиотека для разработчиков

---
+++ heartview/heartview.py
@@ -0,0 +1,14 @@
+def convert(my_name):
+    """
+    Parameters
+    ----------
+    my_name (str): Имя человека
+    
+        Описание функции
+
+    Returns
+    -------
+    None.
+
+    """
+    print(f"Тест {my_name}")
